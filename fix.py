d2350b35bdb60bc974a3c49aab9f55f2b49543fb
Arild Matsson <arild.matsson@gu.se>
2022-01-21 14:53:04 +0100

    Use the Strukna folder

--- fix.py
+++ fix.py
@@ -3,39 +3,47 @@ import re
 from unicodedata import normalize
 
 PATH_CURRENT = '/Users/arildm/University of Gothenburg/Olov Kriström - TTLs/'
+PATH_REMOVED = '/Users/arildm/University of Gothenburg/Olov Kriström - Strukna/'
 
 
 def fn_to_term(fn):
-    m = re.match(r'^- (.*).ttl$', fn)
+    m = re.match(r'^\(?- ([^)]*)\)?.ttl$', fn)
     return normalize('NFC', m.group(1)) if m else None
 
 
 terms = [fn_to_term(fn) for fn in os.listdir(PATH_CURRENT) if fn_to_term(fn)]
+terms_removed = [fn_to_term(fn)
+                 for fn in os.listdir(PATH_REMOVED) if fn_to_term(fn)]
 
 
-def remove_broken_links(fn):
+def remove_broken_links(fn, quiet=False):
     new_content = ''
     with open(os.path.join(PATH_CURRENT, fn)) as f:
         for line in f.readlines():
             m = re.search(r'<http[^>]+/qlit/([^>]+)>', line)
             if m:
                 term = m.group(1)
-                exit
-                if term not in terms:
-                    if term.lower() in [term.lower() for term in terms]:
-                        print('--- Wrong case in: ', fn)
-                        print(line)
-                    else:
-                        print('--- Skipping broken link in: ', fn)
-                        print(line)
-                        continue
+                # Remove a line containing a discarded term
+                if term in terms_removed:
+                    print('Ghost link', fn)
+                    print(line)
+                    continue
+                # Warn about other things
+                if not quiet:
+                    if term not in terms:
+                        if term.lower() in [term.lower() for term in terms]:
+                            print('Warning: Wrong case', fn)
+                            print(line)
+                        else:
+                            print('Warning: Missing link', fn)
+                            print(line)
             new_content += line
     return new_content
 
 
-def remove_broken_links_all(inplace=False):
+def remove_broken_links_all(quiet=False, inplace=False):
     for fn in os.listdir(PATH_CURRENT):
         if fn.endswith('.ttl'):
             os.makedirs('out', exist_ok=True)
             with open('out/' + fn, 'w') as f:
-                f.write(remove_broken_links(fn))
+                f.write(remove_broken_links(fn, quiet))
