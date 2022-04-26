d7e413619bf89c308391e1cd9ecfd35417d8a60c
Arild Matsson <arild.matsson@gu.se>
2022-04-26 11:16:15 +0200

    Doc comments

--- qlit/thesaurus.py
+++ qlit/thesaurus.py
@@ -2,15 +2,27 @@ from rdflib import RDF, SKOS, Graph, URIRef
 
 
 class Termset(Graph):
+    """All the triples for a selected subset of the terms."""
+
     def refs(self) -> list[URIRef]:
+        """The URIRefs of the included terms."""
         return list(self.subjects(RDF.type, SKOS.Concept))
 
 
 class Thesaurus(Termset):
-    def complete_relations(graph: Graph) -> Graph:
+    """An RDF graph indended to contain a full thesaurus."""
+
+    def complete_relations() -> "Thesaurus":
+        """Add triples to ensure that all term-term relations are two-way."""
+        # broader <-> narrower
+        # set inScheme
+        # ajust topConceptOf
+        # topConceptOf <-> hasTopConcept
+        # related <-> related (or not?)
         raise NotImplemented
 
     def terms_if(self, f) -> Termset:
+        """Creates a subset with terms matching some condition."""
         g = Termset()
         for term in self.refs():
             if f(term):
@@ -18,13 +30,17 @@ class Thesaurus(Termset):
         return g
 
     def get_roots(self) -> Termset:
+        """Find all terms without parents."""
         return self.terms_if(lambda term: (term, SKOS.broader, None) not in self)
 
     def get_parents(self, child: URIRef) -> Termset:
+        """Find terms that are directly broader than a given term."""
         return self.terms_if(lambda term: (child, SKOS.broader, term) in self)
 
     def get_children(self, parent: URIRef) -> Termset:
+        """Find terms that are directly narrower than a given term."""
         return self.terms_if(lambda term: (term, SKOS.broader, parent) in self)
 
     def autocomplete(q: str) -> Termset:
+        """Find terms matching a user-given incremental (startswith) search string."""
         raise NotImplemented
